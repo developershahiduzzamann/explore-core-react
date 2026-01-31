6a71a89ded5f260b6c58ff963e638fa6e2b53cd8
developershahiduzzamann <sabbir.mirpur58@gmail.com>
2026-01-31 22:40:23 +0600

    added the jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Vit React</h1>
+      <Person></Person>
+      <Language name ="Sabbir" tech ="JavaScript"></Language>
+      <Language name ="Sabbir" tech ="JavaScript"></Language>
+      
     </>
+  )
+}
+
+function Person() {
+  return(
+    <h1>FontEnd Developer</h1>
+  )
+}
+
+function Language(Props){
+  return(
+    <div style={{
+      border: '1px solid green',
+      borderRadius :'20px',
+      marginTop: '10px',
+    }}>
+      <h2>Name: {Props.name}</h2>
+      <p>Department: {Props.tech}</p>
+    </div>
+    
   )
 }
 
